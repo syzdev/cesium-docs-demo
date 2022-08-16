2f02766c5cedfc59fcb37c5fdd128e7098b0bd3c
syzdev <emailsyz@126.com>
2022-08-16 12:13:01 +0800

    内容更新

--- utils/createButton.js
+++ utils/createButton.js
@@ -1,26 +1,31 @@
-function createButton(link) {
-  // TODO：添加第二个参数mode = light | dark配置不同的字体和背景颜色
+/**
+ * @description: 为页面添加一个按钮
+ * @param {String} link 点击按钮所跳转的链接 _blank
+ * @param {String} mode 按钮明亮模式或黑暗模式
+ * @return {*}
+ */
+function createButton(link, mode) {
   // TODO：使用Object.assign()重写样式属性赋值
-  let myDiv = document.createElement('a')
-  myDiv.innerHTML = 'GitHub'
-  myDiv.style.width = '80px'
-  myDiv.style.textAlign = 'center'
-  myDiv.style.lineHeight = '35px'
-  myDiv.style.height = '35px'
-  myDiv.style.position = 'absolute'
-  myDiv.style.top = '40px'
-  myDiv.style.left = '50px'
-  myDiv.style.borderRadius = '10px'
-  myDiv.style.boxShadow = '0px 10px 14px -7px #cdcdcd'
-  myDiv.style.background = '#000'
-  myDiv.style.color = '#fff'
+  let btnDiv = document.createElement('div')
+  btnDiv.innerHTML = 'GitHub'
+  btnDiv.style.width = '80px'
+  btnDiv.style.textAlign = 'center'
+  btnDiv.style.lineHeight = '35px'
+  btnDiv.style.height = '35px'
+  btnDiv.style.position = 'absolute'
+  btnDiv.style.top = '40px'
+  btnDiv.style.left = '50px'
+  btnDiv.style.borderRadius = '10px'
+  btnDiv.style.boxShadow = '0px 10px 14px -7px #cdcdcd'
+  btnDiv.style.background = mode === 'light' ? '#fff' : '#000'
+  btnDiv.style.color = mode === 'light' ? '#000' : '#fff'
 
-  myDiv.addEventListener('click', () => {
+  btnDiv.addEventListener('click', () => {
     window.open(link)
   })
-  myDiv.addEventListener('mouseover', () => {
-    myDiv.style.cursor = 'pointer'
+  btnDiv.addEventListener('mouseover', () => {
+    btnDiv.style.cursor = 'pointer'
   })
   
-  document.body.appendChild(myDiv)
+  document.body.appendChild(btnDiv)
 }
